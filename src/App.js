e90a1ad2c33be132ac21e1c7f8013c806bd3fe47
petarcorluka86 <petarcorluka86@gmail.com>
2020-09-07 12:26:15 +0200

    novalidate

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
 import React from 'react';
 import './css/App.css';
+import validate from 'validate.js';
+
+const constraints = {
+  fullName: {
+    presence: true
+  },
+  email: {
+    presence: true,
+    email: true
+  },
+  password: {
+    presence: true,
+    length: {
+      minimum: 6
+    }
+  },
+  confirmPassword: {
+    presence: true,
+    equality: {
+      attribute: "password",
+      message: "Password does not match."
+    }
+  }
+};
 
 function App() {
   return (
     <div className="app">
       <div className="form-container">
         <h1>DEMO FORM</h1>
-        <form>
+        <form id="mainForm">
           <label>
             Full name:
-            <input type="text" name="fullName"/>
+            <input id="fullName" type="text" name="fullName"/>
           </label>
           <label>
             Email:
-            <input type="text" name="email"/>
+            <input id="email" type="text" name="email"/>
           </label>
           <label>
             Password:
-            <input type="text" name="password"/>
+            <input id="password" type="password" name="password"/>
           </label>
           <label>
             Confirm password:
-            <input type="text" name="confirmPassword"/>
+            <input id="confirmPassword" type="password" name="confirmPassword"/>
           </label>
-          <button>Submit form</button>
+          <button type="submit">Submit form</button>
         </form>
       </div>
     </div>
   );
 }
+
+validate(document.getElementById("mainForm"),constraints);
 
 export default App;
