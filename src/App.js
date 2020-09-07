23b71af161e5784051c452d5c95b38414aeb2de8
petarcorluka86 <petarcorluka86@gmail.com>
2020-09-07 11:40:44 +0200

    css i jsx dodani

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './css/App.css';
 
 function App() {
   return (
-    <div >
-      App init.
+    <div className="app">
+      <div className="form-container">
+        <h1>DEMO FORM</h1>
+        <form>
+          <label>
+            Full name:
+            <input type="text" name="fullName"/>
+          </label>
+          <label>
+            Email:
+            <input type="text" name="email"/>
+          </label>
+          <label>
+            Password:
+            <input type="text" name="password"/>
+          </label>
+          <label>
+            Confirm password:
+            <input type="text" name="confirmPassword"/>
+          </label>
+          <button>Submit form</button>
+        </form>
+      </div>
     </div>
   );
 }
